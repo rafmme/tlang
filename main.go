e2bb45a288c0f423f9dbee13da0e0a4bb3ee8f14
Timileyin Farayola <rafmme@gmail.com>
2025-04-19 15:19:09 +0100

    Add repl to print out tokenization result

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/rafmme/tlang/repl"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hallo %s! This is the TLang Programming Language!\n", user.Username)
+	fmt.Print("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
