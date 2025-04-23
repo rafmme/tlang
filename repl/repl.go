432444b8a7fb0dfeddfe3e45be653a6da37ea9e2
Timileyin Farayola <rafmme@gmail.com>
2025-04-23 18:54:37 +0100

    Implement evaluation of prefix expressions

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/rafmme/tlang/evaluator"
 	"github.com/rafmme/tlang/lexer"
 	"github.com/rafmme/tlang/parser"
 )
@@ -38,8 +39,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
